2e6d3ea9edcb5add1546c501ec418418bd1690ae
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-10-20 15:16:30 +0200

    Added interface method and documentation to ISimulationStrategy.

--- src/exercise1/simulation/ISimulationStrategy.java
+++ src/exercise1/simulation/ISimulationStrategy.java
@@ -1,4 +1,15 @@
 package exercise1.simulation;
 
+import exercise1.model.Grid;
+
+import java.util.List;
+
 public interface ISimulationStrategy {
+
+    /**
+     * Returns a {@link List} of Grids. Each grid represents one step of the simulation.
+     *
+     * @param grid the initial grid.
+     */
+    List<Grid> getSimulationSteps(Grid grid);
 }
