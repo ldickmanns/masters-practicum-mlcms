85b70b87660628eacbe7c9c4e90d21908c49e1cc
Mahdi Sellami <mahdi.sellami.95@gmail.com>
2019-10-25 15:59:47 +0200

    - Added pedestrian costs in the cost matrix - Added randomness as tie breaker for the move decision

--- src/exercise1/simulation/CostBasedStrategy.java
+++ src/exercise1/simulation/CostBasedStrategy.java
@@ -1,10 +1,13 @@
 package exercise1.simulation;
 
+import exercise1.model.CellStateObjects.Pedestrian;
 import exercise1.model.CellStateObjects.Target;
 import exercise1.model.Direction;
 import exercise1.model.Grid;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static java.lang.Double.min;
 
@@ -16,11 +19,12 @@ public class CostBasedStrategy {
     public CostBasedStrategy(Grid grid) {
         this.grid = grid;
         this.costMatrix = new double[grid.getColumnCount()][grid.getRowCount()];
-        calculateCostMatrix();
+        updateCostMatrix();
     }
 
-    private void calculateCostMatrix() {
+    public void updateCostMatrix() {
         this.costMatrix = calculateEucledianCostMatrix();
+        this.addPedestrianCost();
     }
 
     private double[][] calculateEucledianCostMatrix() {
@@ -38,21 +42,51 @@ public class CostBasedStrategy {
         }
         return costMatrix;
     }
+    
+
+    private void addPedestrianCost() {
+    	List<Pedestrian> pedestrians = grid.getPedestrians();
+    	for (Pedestrian p : pedestrians){
+			costMatrix[p.x][p.y] += 100;
+    	}
+    }
 
     /** Performs one step of the simulation. */
     public void nextStep() {
         grid.getPedestrians().forEach(p -> {
-            double up = costMatrix[p.x][p.y - 1];
-            double right = costMatrix[p.x + 1][p.y];
-            double down = costMatrix[p.x][p.y +1];
-            double left = costMatrix[p.x -1][p.y];
-            double min = min(min(up, right), min(down, left));
-            // Do nothing if distance increases when moving.
-            //if (costMatrix[p.x][p.y] < min) return;
-            if (min == up) grid.movePedestrian(p.x, p.y, Direction.UP);
-            if (min == right) grid.movePedestrian(p.x, p.y, Direction.RIGHT);
-            if (min == down) grid.movePedestrian(p.x, p.y, Direction.DOWN);
-            if (min == left) grid.movePedestrian(p.x, p.y, Direction.LEFT);
+            // Check dynamic cost matrix for the next move
+        	double up = costMatrix[p.x][p.y - 1];
+        	double right = costMatrix[p.x + 1][p.y];
+        	double down = costMatrix[p.x][p.y +1];
+        	double left = costMatrix[p.x -1][p.y];
+        	double min = min(min(up, right), min(down, left));
+            if (costMatrix[p.x][p.y] - 100 < min) return;
+            
+            // Decide for the direction to move to based on the cost matrix
+            // In case of a tie (two or more possible moves are equal to the minimun cost)
+            // We just pick a random move from Set of minima
+            List<String> decisionList = new ArrayList<String>();
+            if (min == up) decisionList.add("UP");
+            if (min == right) decisionList.add("RIGHT");
+            if (min == down) decisionList.add("DOWN");
+            if (min == left) decisionList.add("LEFT");
+    		String decision = (String) decisionList.toArray()[new Random().nextInt(decisionList.size())];
+            
+            switch (decision) {
+	            case "UP":
+	            	grid.movePedestrian(p.x, p.y, Direction.UP);
+	                break;
+	            case "RIGHT":
+	            	grid.movePedestrian(p.x, p.y, Direction.RIGHT);
+	                break;
+	            case "DOWN":
+	            	grid.movePedestrian(p.x, p.y, Direction.DOWN);
+	                break;
+	            case "LEFT":
+	            	grid.movePedestrian(p.x, p.y, Direction.LEFT);
+	                break;
+	        }
+            updateCostMatrix();
         });
     }
 }
