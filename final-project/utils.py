89b9ae900b537626625eca74a9db2a4761c4bbda
Mahdi Sellami <mahdi.sellami.95@gmail.com>
2020-01-21 14:26:15 +0100

    updated files

--- final-project/utils.py
+++ final-project/utils.py
@@ -12,25 +12,23 @@ def euler(ds, alpha, delta, y):
     return y + delta * np.asarray([u, v])
 
 
-def plot_phase_portrait(ds, param, model, area):
+def plot_phase_portrait(ds, param, model, area, x0):
     Y, X = np.mgrid[area:-area:20j, area:-area:20j]
     U, V = ds(X, Y, param)
-    trajectory1 = []
-    trajectory2 = []
-    y1 = [area, -area]
-    y2 = [area, -area]
+    trajectory = []
+    y = x0
     for i in range(20000):
-        trajectory1.append(y1)
-        y1 = euler(ds, param, 0.001, y1)
-        trajectory2.append(y2)
-        y2 = model(torch.FloatTensor(y2).unsqueeze(0), torch.FloatTensor([param]).unsqueeze(0)).tolist()[0]
-    trajectory1 = np.asarray(trajectory1).T
-    trajectory2 = np.asarray(trajectory2).T
+        trajectory.append(y)
+        y = model(torch.FloatTensor(y).unsqueeze(0), torch.FloatTensor([param]).unsqueeze(0)).tolist()[0]
+    trajectory = np.asarray(trajectory).T
+    tspan = (0., 200.)
+    teval = np.arange(tspan[0], tspan[1], 0.01)
+    sol = solve_ivp(lambda t, x: ds(x[0], x[1], param), tspan, x0, t_eval=teval)
     plt.figure(figsize=(5, 5))
     ax = plt.subplot(111)
     ax.streamplot(X, Y, U, V, density=[0.5, 1])
-    ax.plot(trajectory1[0], trajectory1[1])
-    ax.plot(trajectory2[0], trajectory2[1])
+    ax.plot(sol.y[0], sol.y[1])
+    ax.plot(trajectory[0], trajectory[1])
     ax.set_title('Alpha: ' + str(param))
     plt.show()
     return
